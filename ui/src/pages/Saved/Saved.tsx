e1805c0053aa6c5bb2ebe1c3531c1d13215252d1
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-03-08 14:37:59 +0000

    deleted accidental package.json files

--- ui/src/pages/Saved/Saved.tsx
+++ ui/src/pages/Saved/Saved.tsx
@@ -1,57 +1,24 @@
-import React, { useLayoutEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-import "../Saved/Saved.css";
-// import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+import React from "react";
+import data from "../../data.json"; // Import data from JSON file
+import { Person } from "../../util/person";
 
 export const Saved: React.FC = () => {
-  const main = useRef<HTMLDivElement>(null);
-  const smoother = useRef<any>(null);
-
-  const scrollTo = () => {
-    smoother.current.scrollTo(".box-c", true, "center center");
-  };
-  useLayoutEffect(() => {
-    if (main.current) {
-      smoother.current = ScrollSmoother.create({
-        smooth: 2,
-        effects: true,
-      });
-      ScrollTrigger.create({
-        trigger: ".box-c",
-        pin: true,
-        start: "center center",
-        end: "+=300",
-        markers: true,
-      });
-    }
-  }, []);
-
   return (
     <>
-      <div id="smooth-wrapper" ref={main}>
-        <div id="smooth-content">
-          <header className="header">
-            <h2 className="title">GSAP ScrollSmoother in React</h2>
-            <button className="button" onClick={scrollTo}>
-              Jump to C
-            </button>
-          </header>
-          <div className="box box-a gradient-blue" data-speed="0.5">
-            a
+      <div className="card-ctn flex h-full overflow-x-auto">
+        {data.map((savedPerson: Person, index: number) => (
+          <div
+            key={index}
+            className="card w-64 border bg-white rounded-lg shadow-md mx-2"
+          >
+            <p>age: {savedPerson.age}</p>
+            <p>job title: {savedPerson.job_title}</p>
+            <p>instagram: {savedPerson.instagram}</p>
           </div>
-          <div className="box box-b gradient-orange" data-speed="0.8">
-            b
-          </div>
-          <div className="box box-c gradient-purple" data-speed="1.5">
-            c
-          </div>
-          <div className="line"></div>
-        </div>
+        ))}
       </div>
     </>
   );
 };
+
+export default Saved;
