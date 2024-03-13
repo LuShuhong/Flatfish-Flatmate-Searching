b40d050377115159c384abbea778986aff2e27ac
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-03-13 11:00:41 +0000

    commit before merge

--- ui/src/pages/Saved/Saved.tsx
+++ ui/src/pages/Saved/Saved.tsx
@@ -7,18 +7,18 @@ export const Saved: React.FC = () => {
   return (
     <>
       <div className="card-ctn ">
-        {data.map((savedPerson: Profile, index: number) => (
+        {data.map((savedProfile: Profile, index: number) => (
           <>
             <div className="flex justify-center align-center h-full w-full">
               <Cards
-                userId={savedPerson.userId}
-                name={savedPerson.name}
-                age={savedPerson.age} // Parse age as a number
-                jobTitle={savedPerson.jobTitle}
-                description={savedPerson.description}
-                email={savedPerson.email}
-                userGender={savedPerson.userGender}
-                userInsta={savedPerson.userInsta}
+                userId={savedProfile.userId}
+                name={savedProfile.name}
+                age={savedProfile.age} // Parse age as a number
+                jobTitle={savedProfile.jobTitle}
+                description={savedProfile.description}
+                email={savedProfile.email}
+                userGender={savedProfile.userGender}
+                userInsta={savedProfile.userInsta}
               />
             </div>
           </>
