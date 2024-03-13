a28037723e334096fc4b52fc1a332c71a717100e
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-03-13 09:56:48 +0000

    Add birthday input

--- ui/src/components/BirthdayInput/BirthdayInput.tsx
+++ ui/src/components/BirthdayInput/BirthdayInput.tsx
@@ -1,8 +1,12 @@
 export const BirthdayInput: React.FC = () => {
   return (
     <div className="h-1/5 w-full">
-      <label className="text-xs w-full">
-        <input type="date" className="" />
+      <label className="flex flex-col text-xs w-full">
+        birthday
+        <input
+          type="date"
+          className="w-2/5 h-55% p-3 text-xs bg-[#E5E5E5] border border-gray-400 rounded-xl mt-1"
+        />
       </label>
     </div>
   );
