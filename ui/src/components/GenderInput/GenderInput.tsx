4d5cfc234100486eba74bfabcd590a9dc75efb52
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-03-13 15:43:31 +0000

    Fix default value issues

--- ui/src/components/GenderInput/GenderInput.tsx
+++ ui/src/components/GenderInput/GenderInput.tsx
@@ -10,15 +10,12 @@ export const GenderInput: React.FC<Props> = ({ gender, handleUserGender }) => {
         gender
         <select
           className="bg-[#E5E5E5] border border-gray-400 rounded-xl mt-1 text-lg p-2"
+          defaultValue={gender}
           onChange={(e) => handleUserGender(e.target.value)}
         >
           <option value="select">select</option>
-          <option value="m" selected={gender === "m"}>
-            male
-          </option>
-          <option value="f" selected={gender === "f"}>
-            female
-          </option>
+          <option value="m">male</option>
+          <option value="f">female</option>
         </select>
       </label>
     </div>
