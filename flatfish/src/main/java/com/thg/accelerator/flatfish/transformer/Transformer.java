2accf0bccc8d261126fad2fb5fd1ad3386f6ee46
Nattawat Ngowthammatas <nattawat.ngowthammatas28@thehutgroup.com>
2024-03-20 08:39:32 +0000

    fixed bidirectional database error

--- flatfish/src/main/java/com/thg/accelerator/flatfish/transformer/Transformer.java
+++ flatfish/src/main/java/com/thg/accelerator/flatfish/transformer/Transformer.java
@@ -10,16 +10,37 @@ public class Transformer {
         return new UserDto(userEntity.getUserId(), userEntity.getName(), userEntity.getBirthday(), userEntity.getAge(),
                 userEntity.getDescription(), userEntity.getUserGender(), userEntity.getInstagram(),
                 userEntity.getBudgetMin(), userEntity.getBudgetMax(), userEntity.getAgeMin(),
-                userEntity.getAgeMax(), userEntity.getGender(),userEntity.getSavedProfilesEntitiesList(), userEntity.getLocation1(), userEntity.getLocation2(),
+                userEntity.getAgeMax(), userEntity.getGender(), userEntity.getLocation1(), userEntity.getLocation2(),
                 userEntity.getLocation3());
     }
 
+//    public static UserEntity transformUserDtoToEntity(UserDto userDto) {
+//        return new UserEntity(userDto.getUserId(), userDto.getName(), userDto.getBirthday(), userDto.getAge(),
+//                userDto.getDescription(), userDto.getUserGender(), userDto.getInstagram(),
+//                userDto.getBudgetMin(), userDto.getBudgetMax(), userDto.getAgeMin(),
+//                userDto.getAgeMax(), userDto.getGender(),userDto.getSavedProfileEntities(), userDto.getLocation1(), userDto.getLocation2(),
+//                userDto.getLocation3());
+//    }
+
     public static UserEntity transformUserDtoToEntity(UserDto userDto) {
-        return new UserEntity(userDto.getUserId(), userDto.getName(), userDto.getBirthday(), userDto.getAge(),
-                userDto.getDescription(), userDto.getUserGender(), userDto.getInstagram(),
-                userDto.getBudgetMin(), userDto.getBudgetMax(), userDto.getAgeMin(),
-                userDto.getAgeMax(), userDto.getGender(),userDto.getSavedProfileEntities(), userDto.getLocation1(), userDto.getLocation2(),
-                userDto.getLocation3());
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(userDto.getUserId());
+        userEntity.setName(userDto.getName());
+        userEntity.setBirthday(userDto.getBirthday());
+        userEntity.setAge(userDto.getAge());
+        userEntity.setDescription(userDto.getDescription());
+        userEntity.setUserGender(userDto.getUserGender());
+        userEntity.setInstagram(userDto.getInstagram());
+        userEntity.setBudgetMin(userDto.getBudgetMin());
+        userEntity.setBudgetMax(userDto.getBudgetMax());
+        userEntity.setAgeMin(userDto.getAgeMin());
+        userEntity.setAgeMax(userDto.getAgeMax());
+        userEntity.setGender(userDto.getGender());
+        // userEntity.setSavedProfilesEntitiesList(convertSavedProfiles(userDto.getSavedProfileEntities()));
+        userEntity.setLocation1(userDto.getLocation1());
+        userEntity.setLocation2(userDto.getLocation2());
+        userEntity.setLocation3(userDto.getLocation3());
+        return userEntity;
     }
 }
 
