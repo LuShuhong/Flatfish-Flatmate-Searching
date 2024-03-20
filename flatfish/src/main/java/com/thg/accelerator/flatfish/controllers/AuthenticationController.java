520628c918f2f4846c546ff01e0ef0037787fdab
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-03-20 22:42:36 +0000

    Change auth controller

--- flatfish/src/main/java/com/thg/accelerator/flatfish/controllers/AuthenticationController.java
+++ flatfish/src/main/java/com/thg/accelerator/flatfish/controllers/AuthenticationController.java
@@ -6,14 +6,12 @@ import com.thg.accelerator.flatfish.entities.UserEntity;
 import com.thg.accelerator.flatfish.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:3000")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
