82b57ad66b1fe07294c76a69ded786f5718d4932
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-03-25 16:26:07 +0000

    Resize image

--- ui/src/pages/ProfilePage/components/ProfilePic/ProfilePic.tsx
+++ ui/src/pages/ProfilePage/components/ProfilePic/ProfilePic.tsx
@@ -9,7 +9,7 @@ export const ProfilePic: React.FC<Props> = ({ pic, handleImageChange }) => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="flex items-center justify-center w-28 h-28 rounded-1/2 border overflow-hidden">
-        <img src={pic} alt="profile" className="w-10/12"></img>
+        <img src={pic} alt="profile" className=""></img>
       </div>
       <label className="ml-2">
         <div className="">
