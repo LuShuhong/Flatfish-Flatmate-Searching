bbcb1d091e0b23be0292319e5ac8bd0179a2dc61
Jason Guan <Jason.Guan21@thehutgroup.com>
2024-03-25 21:55:50 +0000

    Fix bug

--- ui/src/requests/putRequests.ts
+++ ui/src/requests/putRequests.ts
@@ -1,13 +1,11 @@
 import { PutBody } from "../util/interfaces/PutBody";
 
 export const put = (url: string, body: PutBody | object) => {
-  fetch(url, {
+  return fetch(url, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(body),
-  })
-    .then(() => console.log("success"))
-    .catch((error) => console.log(error));
+  });
 };
